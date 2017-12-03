b8ddecfbdfa5e0d7ef4d9663090cc420f296fb1c
Per Malmberg <per.malmberg@gmail.com>
2017-12-03 12:10:59 +0100

    Both ADC read. WS2812B active.

--- main/G2Alarm.cpp
+++ main/G2Alarm.cpp
@@ -18,23 +18,28 @@ using namespace smooth::application::io;
 
 static const gpio_num_t DIGITAL_CHANGE_PIN = GPIO_NUM_33;
 static const gpio_num_t ANALOG_CHANGE_PIN_1 = GPIO_NUM_34;
+static const gpio_num_t ANALOG_CHANGE_PIN_2 = GPIO_NUM_35;
 
 G2Alarm::G2Alarm()
-        : Application(5, milliseconds(1000)),
+        : Application(5, milliseconds(200)),
           i2c_power(GPIO_NUM_27, true, false, true),
+          level_shifter_enable(GPIO_NUM_5, true, false, true),
           digital_i2c_master(I2C_NUM_0, GPIO_NUM_16, false, GPIO_NUM_17, false, 100000),
           analog_i2c_master(I2C_NUM_1, GPIO_NUM_25, false, GPIO_NUM_26, false, 1000000),
           input_change_queue(*this, *this),
-          analog_change_queue(*this, *this),
+          analog_change_queue_1(*this, *this),
+          analog_change_queue_2(*this, *this),
           digital_input_change(input_change_queue, DIGITAL_CHANGE_PIN, false, false, GPIO_INTR_ANYEDGE),
-          analog_change_1(analog_change_queue, ANALOG_CHANGE_PIN_1, false, false, GPIO_INTR_NEGEDGE)
+          analog_change_1(analog_change_queue_1, ANALOG_CHANGE_PIN_1, false, false, GPIO_INTR_NEGEDGE),
+          analog_change_2(analog_change_queue_2, ANALOG_CHANGE_PIN_2, false, false, GPIO_INTR_NEGEDGE),
+          rgb(RMT_CHANNEL_0, GPIO_NUM_2, 5, smooth::application::rgb_led::WS2812B())
 {
 }
 
 void G2Alarm::init()
 {
-    i2c_power.clr();
-    std::this_thread::sleep_for(milliseconds(5));
+    smooth::core::Application::init();
+    level_shifter_enable.set();
     i2c_power.set();
 
     digital_io = digital_i2c_master.create_device<MCP23017>(0x20);
@@ -75,14 +80,37 @@ void G2Alarm::init()
     }
 
     cycler_1 = make_unique<AnalogCycler>(analog_i2c_master.create_device<ADS1115>(0x48));
+    cycler_2 = make_unique<AnalogCycler>(analog_i2c_master.create_device<ADS1115>(0x49));
 
     // Read inputs once on startup to clear waiting interrupts on the i2c devices.
     update_inputs();
+
+    // get the ball rolling
+    cycler_1->trigger_read();
+    cycler_2->trigger_read();
 }
 
 void G2Alarm::tick()
 {
-    cycler_1->trigger_read();
+    if (++out == 8)
+    {
+        out = 0;
+    }
+    digital_io->set_output(MCP23017::Port::B, static_cast<uint8_t>(1 << out));
+
+    if (++rgb_count == 5)
+    {
+        rgb_count = 0;
+    }
+
+    for (uint16_t i = 0; i < 5; ++i)
+    {
+        rgb.set_pixel(i, 0, 0, 0);
+    }
+
+    rgb.set_pixel(rgb_count, 0x45, 0x34, 0x00 );
+
+    rgb.apply();
 }
 
 void G2Alarm::event(const smooth::core::io::InterruptInputEvent& ev)
@@ -101,8 +129,20 @@ void G2Alarm::event(const smooth::core::io::InterruptInputEvent& ev)
     else if (ev.get_io() == ANALOG_CHANGE_PIN_1)
     {
         uint16_t result = cycler_1->get_value();
-        Log::info("Analog", Format("{1} {2}", Int32(cycler_1->get_input_number()), UInt32(result)));
+        Log::info("Analog1", Format("{1} {2}", Int32(cycler_1->get_input_number()), UInt32(result)));
         cycler_1->cycle();
+
+        // Trigger read of other device
+        cycler_2->trigger_read();
+    }
+    else if (ev.get_io() == ANALOG_CHANGE_PIN_2)
+    {
+        uint16_t result = cycler_2->get_value();
+        Log::info("Analog2", Format("{1} {2}", Int32(cycler_2->get_input_number()), UInt32(result)));
+        cycler_2->cycle();
+
+        // Trigger read of other device
+        cycler_1->trigger_read();
     }
 }
 
@@ -111,4 +151,5 @@ void G2Alarm::update_inputs()
     uint8_t digital;
     digital_io->read_input(MCP23017::Port::A, digital);
     cycler_1->cycle();
+    cycler_2->cycle();
 }
--- main/G2Alarm.h
+++ main/G2Alarm.h
@@ -9,6 +9,7 @@
 #include <smooth/core/io/InterruptInput.h>
 #include <smooth/application/io/MCP23017.h>
 #include <smooth/application/io/ADS1115.h>
+#include <smooth/application/rgb_led/RGBLed.h>
 #include "AnalogCycler.h"
 
 class G2Alarm
@@ -24,14 +25,22 @@ class G2Alarm
 
     private:
         smooth::core::io::Output i2c_power;
+        smooth::core::io::Output level_shifter_enable;
         smooth::core::io::i2c::Master digital_i2c_master;
         smooth::core::io::i2c::Master analog_i2c_master;
         std::unique_ptr<smooth::application::io::MCP23017> digital_io{};
         smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 5> input_change_queue;
-        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 5> analog_change_queue;
+        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 5> analog_change_queue_1;
+        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 5> analog_change_queue_2;
         smooth::core::io::InterruptInput digital_input_change;
         smooth::core::io::InterruptInput analog_change_1;
+        smooth::core::io::InterruptInput analog_change_2;
         std::unique_ptr<AnalogCycler> cycler_1{};
+        std::unique_ptr<AnalogCycler> cycler_2{};
+        smooth::application::rgb_led::RGBLed rgb;
+
+        uint8_t out = 0;
+        uint16_t rgb_count = 0;
 
         void update_inputs();
 };
