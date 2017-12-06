8bf05f148e787449df073ef43e589b6de46b77c1
Per Malmberg <per.malmberg@gmail.com>
2017-12-06 21:11:31 +0100

    Start of Wiegand.

--- main/Wiegand.cpp
+++ main/Wiegand.cpp
@@ -3,19 +3,62 @@
 //
 
 #include "Wiegand.h"
+#include <chrono>
+#include <string>
 #include <smooth/core/logging/log.h>
+#include <smooth/core/util/ByteSet.h>
 
+using namespace std::chrono;
 using namespace smooth::core::logging;
+using namespace smooth::core::timer;
+using namespace smooth::core::util;
+
+static const gpio_num_t D0 = GPIO_NUM_0;
+static const gpio_num_t D1 = GPIO_NUM_4;
+static const std::chrono::milliseconds timeout(25);
 
 Wiegand::Wiegand(smooth::core::Task& task)
-        : d1_queue(task, *this),
-          d2_queue(task, *this),
-          d1(d1_queue, GPIO_NUM_0, false, false, GPIO_INTR_NEGEDGE),
-          d2(d2_queue, GPIO_NUM_4, false, false, GPIO_INTR_NEGEDGE)
+        : d0_queue(task, *this),
+          d1_queue(task, *this),
+          d0(d0_queue, GPIO_NUM_0, false, false, GPIO_INTR_NEGEDGE),
+          d1(d1_queue, GPIO_NUM_4, false, false, GPIO_INTR_NEGEDGE),
+          line_silent("WiegandLineSilent", 5, task, *this),
+          expire(Timer::create("WiegandLineSilent", 1, line_silent, false, milliseconds(25)))
 {
 }
 
 void Wiegand::event(const smooth::core::io::InterruptInputEvent& event)
 {
-    Log::info("W", Format("{1}", UInt32(event.get_io())));
+    expire->start(timeout);
+    bit_count++;
+    data <<= 1;
+    data.set(data.size() - 1, event.get_io() == D1);
+    Log::info("event", Format("{1}", UInt32(bit_count)));
+}
+
+void Wiegand::event(const smooth::core::timer::TimerExpiredEvent& event)
+{
+    Log::info("Wiegand", Format("{1}", UInt32(bit_count)));
+
+    bit_count = 0;
+
+    if (bit_count == 8)
+    {
+        // Keyboard data
+        auto high = data.to_ulong() & 0x0F;
+        auto low = data.to_ulong() & 0xF0;
+
+        Log::info("QQQ", Format("Low: {1}, High: {2}, ~Low: {3}", UInt32(low), UInt32(high), UInt32(~low)));
+
+        if (high == ~low)
+        {
+
+        }
+
+    }
+
+
+    data.reset();
+
+
 }
--- main/Wiegand.h
+++ main/Wiegand.h
@@ -1,20 +1,29 @@
 #pragma once
 
+#include <bitset>
 #include <smooth/core/Task.h>
 #include <smooth/core/io/InterruptInput.h>
 #include <smooth/core/ipc/IEventListener.h>
 #include <smooth/core/ipc/ISRTaskEventQueue.h>
 #include <smooth/core/ipc/ITaskEventQueue.h>
+#include <smooth/core/timer/Timer.h>
+#include <memory>
 
-class Wiegand : public smooth::core::ipc::IEventListener<smooth::core::io::InterruptInputEvent>
+class Wiegand : public smooth::core::ipc::IEventListener<smooth::core::io::InterruptInputEvent>,
+                public smooth::core::ipc::IEventListener<smooth::core::timer::TimerExpiredEvent>
 {
     public:
         Wiegand(smooth::core::Task& task);
         void event(const smooth::core::io::InterruptInputEvent& event) override;
+        void event(const smooth::core::timer::TimerExpiredEvent& event) override;
 
     private:
-        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 20> d1_queue;
-        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 20> d2_queue;
+        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 40> d0_queue;
+        smooth::core::ipc::ISRTaskEventQueue<smooth::core::io::InterruptInputEvent, 40> d1_queue;
+        smooth::core::io::InterruptInput d0;
         smooth::core::io::InterruptInput d1;
-        smooth::core::io::InterruptInput d2;
+        std::bitset<34> data{};
+        uint8_t bit_count = 0;
+        smooth::core::ipc::TaskEventQueue<smooth::core::timer::TimerExpiredEvent> line_silent;
+        std::shared_ptr<smooth::core::timer::Timer> expire;
 };
