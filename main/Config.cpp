825e929f5221432bae344255a44d6a928c5bbb84
Per Malmberg <per.malmberg@gmail.com>
2017-12-25 13:08:08 +0100

    More config read

--- main/Config.cpp
+++ main/Config.cpp
@@ -4,12 +4,17 @@
 
 #include <sstream>
 #include "Config.h"
-#include <smooth/core/json/Json.h>
+#include <smooth/core/json/Value.h>
 #include <smooth/core/logging/log.h>
 
 
 using namespace smooth::core::json;
 using namespace smooth::core::logging;
+
+std::string Config::get_custom_name(const std::string& short_name)
+{
+    return io_names[short_name];
+}
 
 bool Config::parse(const std::string& data)
 {
@@ -21,46 +26,63 @@ bool Config::parse(const std::string& data)
 
     if (root)
     {
+        const int analog_input_count = 8;
+        const int digital_input_count = 8;
+        const int digital_output_count = 8;
+
         Value v(root);
 
-        Log::info("Config", Format("Data prepared, reading analog input names"));
-        auto analog_names = v["io"]["naming"]["analog"];
+        Log::info("Config", Format("Reading analog input details"));
 
-        for (auto i = 0; i < 8; ++i)
+        for (auto i = 0; i < analog_input_count; ++i)
         {
             std::stringstream ss;
             ss << "a" <<  i;
-            auto name = analog_names[ss.str()];
+            auto name = v["io"]["naming"]["analog"][ss.str()];
             io_names[ss.str()] = name.get_string(ss.str());
             Log::info("Analog", Format("Will use name '{1}' for analog input {2}", Str(io_names[ss.str()]), Str(ss.str())));
+
+            auto ref_root = v["io"]["reference_values"]["analog"][ss.str()];
+            auto ref_value = ref_root["ref"];
+            auto ref_variance = ref_root["variance"];
+
+            analog_ref[ss.str()] = AnalogRef{ref_value.get_int(0), ref_value.get_int(0)};
+            Log::info("Analog", Format("{1} ref value: {2}, variance: {3}",
+                                       Str(get_custom_name(ss.str())),
+                                       Int32(ref_value.get_int(0)),
+                                       Int32(ref_variance.get_int(0))));
         }
 
-        Log::info("Config", Format("Data prepared, reading digital input names"));
-        auto digital_name_input = v["io"]["naming"]["digital"]["input"];
+        Log::info("Config", Format("Reading digital input details"));
 
-        for (auto i = 0; i < 8; ++i)
+        for (auto i = 0; i < digital_input_count; ++i)
         {
             std::stringstream ss;
             ss << "i" <<  i;
-            auto name = digital_name_input[ss.str()];
+            auto name = v["io"]["naming"]["digital"]["input"][ss.str()];
             io_names[ss.str()] = name.get_string(ss.str());
-            Log::info("Digital input", Format("Will use name '{1}' for digital input {2}", Str(io_names[ss.str()]), Str(ss.str())));
+            Log::info("Digital input", Format("Will use name '{1}' for digital input {2}",
+                                              Str(get_custom_name(ss.str())),
+                                              Str(ss.str())));
+
+            auto ref = v["io"]["reference_values"]["digital"][ss.str()]["ref"];
+
+            digital_ref[ss.str()] = ref.get_bool(false);
+            Log::info("Digital", Format("{1} ref value: {2}",
+                                       Str(get_custom_name(ss.str())),
+                                       Bool(digital_ref[ss.str()])));
         }
 
+        Log::info("Config", Format("Reading digital output details"));
 
-
-        Log::info("Config", Format("Data prepared, reading digital output names"));
-        auto digital_name_output = v["io"]["naming"]["digital"]["output"];
-
-        for (auto i = 0; i < 8; ++i)
+        for (auto i = 0; i < digital_output_count; ++i)
         {
             std::stringstream ss;
             ss << "o" <<  i;
-            auto name = digital_name_output[ss.str()];
+            auto name = v["io"]["naming"]["digital"]["output"][ss.str()];
             io_names[ss.str()] = name.get_string(ss.str());
             Log::info("Digital out", Format("Will use name '{1}' for digital output {2}", Str(io_names[ss.str()]), Str(ss.str())));
         }
-
 
         cJSON_Delete(root);
 
--- main/Config.h
+++ main/Config.h
@@ -6,7 +6,23 @@
 class Config
 {
     public:
+        struct AnalogRef
+        {
+            AnalogRef() = default;
+
+            AnalogRef(int v, int var) : value(v), variance(var)
+            {
+            }
+
+            int value = 0;
+            int variance = 0;
+        };
+
         bool parse(const std::string& data);
+        std::string get_custom_name(const std::string& short_name);
+
     private:
-        std::unordered_map<std::string,std::string> io_names{};
+        std::unordered_map<std::string, std::string> io_names{};
+        std::unordered_map<std::string, AnalogRef> analog_ref{};
+        std::unordered_map<std::string, bool> digital_ref{};
 };
